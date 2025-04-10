15f83c59d0b1a976cdbfd167acc84f260e9975a1
Maja Sigfeldt <msigfeldt@gmail.com>
2025-04-10 14:42:43 +0200

    try to implement stored procedure

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
+using System.Data;
 namespace ado_sakila_testv15
 {
     internal class Program
@@ -10,29 +11,44 @@ namespace ado_sakila_testv15
         {
             var connection= new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Sakila; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
 
-            Console.WriteLine("Välj om du vill söka på \n  1. Förnamn \n  2. Efternamn");
+            Console.WriteLine("Välj om du vill söka på \n  1. Förnamn \n  2. Efternamn\n\n");
             int choice = int.Parse(Console.ReadLine());
             if (choice == 1)
             {
-                //förnamn
-                //var command1 = new SqlCommand("förnamn", connection); //här ska sql uttryck läggas in
-                //connection.Open(); //öppnar koppling till db
+                Console.WriteLine("Ange ett förnamn:");
+                string firstname = Console.ReadLine();
 
-                //var returninfo = command1.ExecuteReader();
 
-                //connection.Close(); //Stänger min koppling till db
+                var command1 = new SqlCommand("uspSearchForActors_FirstName", connection); //här ska sql uttryck läggas in
+                //Säg att det är en SP
+                command1.CommandType = CommandType.StoredProcedure;
+                //skapa en parameter för command/SP
+                var param = new SqlParameter(firstname, SqlDbType.NVarChar);
+                //Lägg till parametern i kommandot
+                command1.Parameters.Add(param);
+
+
+
+                connection.Open(); //öppnar koppling till db
+
+                var returninfo = command1.ExecuteReader();
+
+                connection.Close(); //Stänger min koppling till db
+
+
             }
             else if (choice == 2)
             {
-                //efternamn
-                //var command2 = new SqlCommand("efternamn", connection); //här ska sql uttryck läggas in
-                //connection.Open(); //öppnar koppling till db
+                Console.WriteLine("Ange ett efternamn:");
+                string lastname = Console.ReadLine();
+                var command2 = new SqlCommand("efternamn", connection); //här ska sql uttryck läggas in
+                connection.Open(); //öppnar koppling till db
 
-                //var returninfo = command2.ExecuteReader();
+                var returninfo = command2.ExecuteReader();
 
-                //connection.Close(); //Stänger min koppling till db
+                connection.Close(); //Stänger min koppling till db
             }
-            else 
+            else
             {
                 Console.WriteLine("Ogiltigt val. Programmet avslutas");
             }
