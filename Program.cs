ce64714f8a3793d7109d15735662d6d9d7cbf051
Maja Sigfeldt <msigfeldt@gmail.com>
2025-04-10 11:14:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+namespace ado_sakila_testv15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
