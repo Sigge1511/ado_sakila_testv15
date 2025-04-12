2cef8ca2be93ba4059d791080b1cae14b431fbe5
Maja Sigfeldt <msigfeldt@gmail.com>
2025-04-12 12:50:35 +0200

    fixed my parameters and reader

--- Program.cs
+++ Program.cs
@@ -3,10 +3,14 @@ using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
 using System.Data;
+using MySql.Data.MySqlClient;
 namespace ado_sakila_testv15
 {
     internal class Program
     {
+        //uspSearchFilmsByActorId
+        //uspSearchForActors_LastName
+        //uspSearchForActors_FirstName
         static void Main(string[] args)
         {
             var connection= new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Sakila; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
@@ -17,25 +21,26 @@ namespace ado_sakila_testv15
             {
                 Console.WriteLine("Ange ett förnamn:");
                 string firstname = Console.ReadLine();
-
-
-                var command1 = new SqlCommand("uspSearchForActors_FirstName", connection); //här ska sql uttryck läggas in
-                //Säg att det är en SP
-                command1.CommandType = CommandType.StoredProcedure;
-                //skapa en parameter för command/SP
-                var param = new SqlParameter(firstname, SqlDbType.NVarChar);
-                //Lägg till parametern i kommandot
-                command1.Parameters.Add(param);
-
-
-
+                //öppna
                 connection.Open(); //öppnar koppling till db
+                //använd kommando
+                using (var command1 = new SqlCommand("uspSearchForActors_FirstName", connection)) //här ska sql uttryck läggas in
+                {
+                    //Säg att det är en SP
+                    command1.CommandType = CommandType.StoredProcedure;
+                    //skicka med parameter
+                    command1.Parameters.AddWithValue("@FirstName", firstname);
 
-                var returninfo = command1.ExecuteReader();
-
+                    //Sätter igång min reader för att hämta data
+                    using (SqlDataReader reader = command1.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"ID: {reader["actor_id"]}, Förnamn: {reader["first_name"]}, Efternamn: {reader["last_name"]}");
+                        }
+                    }
+                }
                 connection.Close(); //Stänger min koppling till db
-
-
             }
             else if (choice == 2)
             {
