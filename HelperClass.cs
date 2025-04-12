0c60cf08f60fdaa60c682af159db750f3a824aa2
Maja Sigfeldt <msigfeldt@gmail.com>
2025-04-12 13:24:55 +0200

    started with sorting into methods

---
+++ HelperClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace ado_sakila_testv15
+{
+    internal class HelperClass
+    {
+        internal void Menu() 
+        {
+            Console.WriteLine("Meny");
+            Console.WriteLine("1. Sök efter förnamn");
+            Console.WriteLine("2. Sök efter efternamn");
+            Console.WriteLine("Välj genom att skriva en siffra");
+            int choice = int.Parse(Console.ReadLine());
+            MenuSelection(choice);
+        }
+        internal void MenuSelection(int choice) 
+        {
+            switch (choice) 
+            {
+                case 1:
+                    Console.Clear();
+                    SearchByFirstName();
+                    break;
+                case 2:
+                    Console.Clear();
+                    SearchByLastName();
+                    break;
+                default:
+
+                    break;
+            
+            }
+        }
+        internal void SearchByFirstName()
+        {
+            var connection = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Sakila; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+
+            Console.WriteLine("Ange ett förnamn:");
+            string firstname = Console.ReadLine();
+            //öppna
+            connection.Open(); //öppnar koppling till db
+                               //använd kommando
+            using (var command1 = new SqlCommand("uspSearchForActors_FirstName", connection)) //här står namn på min SP
+            {
+                //Säg att det är en SP
+                command1.CommandType = CommandType.StoredProcedure;
+                //skicka med parameter
+                command1.Parameters.AddWithValue("@FirstName", firstname);
+
+                //Sätter igång min reader för att hämta data
+                using (SqlDataReader reader = command1.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader["Actor_Id"]}, Förnamn: {reader["FirstName"]}, Efternamn: {reader["LastName"]}");
+                    }
+                }
+                SearchFilmsByActorId();
+            }
+            connection.Close(); //Stänger min koppling till db
+        }
+        internal void SearchByLastName()
+        {
+            var connection = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Sakila; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+
+            Console.WriteLine("Ange ett efternamn:");
+            string lastname = Console.ReadLine();
+            //öppna
+            connection.Open(); //öppnar koppling till db
+                               //använd kommando
+            using (var command2 = new SqlCommand("uspSearchForActors_LastName", connection)) //här står namn på min SP
+            {
+                //Säg att det är en SP
+                command2.CommandType = CommandType.StoredProcedure;
+                //skicka med parameter
+                command2.Parameters.AddWithValue("@LastName", lastname);
+
+                //Sätter igång min reader för att hämta data
+                using (SqlDataReader reader = command2.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader["Actor_Id"]}, Förnamn: {reader["FirstName"]}, Efternamn: {reader["LastName"]}");
+                    }
+                }
+                SearchFilmsByActorId();
+            }
+        }
+        internal void SearchFilmsByActorId() 
+        { 
+        
+        
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
 using System.Data;
-using MySql.Data.MySqlClient;
 namespace ado_sakila_testv15
 {
     internal class Program
@@ -13,34 +10,12 @@ namespace ado_sakila_testv15
         //uspSearchForActors_FirstName
         static void Main(string[] args)
         {
-            var connection= new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Sakila; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+            var helper = new HelperClass();
+            helper.Menu();
 
-            Console.WriteLine("Välj om du vill söka på \n  1. Förnamn \n  2. Efternamn\n\n");
-            int choice = int.Parse(Console.ReadLine());
-            if (choice == 1)
-            {
-                Console.WriteLine("Ange ett förnamn:");
-                string firstname = Console.ReadLine();
-                //öppna
-                connection.Open(); //öppnar koppling till db
-                //använd kommando
-                using (var command1 = new SqlCommand("uspSearchForActors_FirstName", connection)) //här ska sql uttryck läggas in
-                {
-                    //Säg att det är en SP
-                    command1.CommandType = CommandType.StoredProcedure;
-                    //skicka med parameter
-                    command1.Parameters.AddWithValue("@FirstName", firstname);
 
-                    //Sätter igång min reader för att hämta data
-                    using (SqlDataReader reader = command1.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Console.WriteLine($"ID: {reader["Actor_Id"]}, Förnamn: {reader["FirstName"]}, Efternamn: {reader["LastName"]}");
-                        }
-                    }
-                }
-                connection.Close(); //Stänger min koppling till db
+
+            
             }
             else if (choice == 2)
             {
